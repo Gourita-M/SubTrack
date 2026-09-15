ca705b40680a7afa2866aa2dad6c43a6baa9d5ee
Gourita-M <mouad.gourita@gmail.com>
2026-09-15 09:23:43 +0100

    add SubscriptionService class for managing subscription operations

---
+++ src/main/java/services/SubscriptionService.java
@@ -0,0 +1,21 @@
+package services;
+
+public class SubscriptionService {
+    
+    public void create()
+    {
+
+    }
+    public void modification()
+    {
+
+    }
+    public void delete()
+    {
+
+    }
+    public void finished()
+    {
+        
+    }
+}

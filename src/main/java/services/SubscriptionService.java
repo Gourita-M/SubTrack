2b17ff24d3cffb07cdc16cd71d7df9ae8cabf68b
Gourita-M <mouad.gourita@gmail.com>
2026-09-15 17:28:24 +0100

    refactor createWithCommitment method to accept parameters and improve date handling

--- src/main/java/services/SubscriptionService.java
+++ src/main/java/services/SubscriptionService.java
@@ -1,12 +1,30 @@
 package services;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import entity.SubsctiptionWithCommitment;
 
 public class SubscriptionService {
     
-    public void createWithCommitment()
+    public static void createWithCommitment(String serviceName, String monthlyAmount, String startDat, String endDat)
     {
-        //SubsctiptionWithCommitment subscribe = new SubsctiptionWithCommitment(null, null, null, null, null, 0)
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yy");
+
+        try{
+            Date startDate = format.parse(startDat);
+            System.err.println("Enter Your Service End Date (mm/dd/yy): ");
+            Date endDate = format.parse(endDat);
+
+            SubsctiptionWithCommitment subscribe = new SubsctiptionWithCommitment(serviceName, monthlyAmount, startDate, endDate, Status.Active, 5);
+
+            } catch(Exception e){
+
+                System.out.println("Invalid Date Formate");
+
+            }
+
+        
+
     }
     public void createWithoutCommitment()
     {

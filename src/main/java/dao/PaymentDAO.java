0cc1335ee8772a1fc97c8e835ba7b5d58a50d966
Gourita-M <mouad.gourita@gmail.com>
2026-09-15 09:21:56 +0100

    add PaymentDAO class with method stubs for payment operations

---
+++ src/main/java/dao/PaymentDAO.java
@@ -0,0 +1,36 @@
+package dao;
+
+public class PaymentDAO {
+
+    public void createPayment(){
+
+    }   
+    public void findById(int id)
+    {
+
+    }
+    public void findBySubscription()
+    {
+        
+    }
+    public void findAll()
+    {
+
+    }
+    public void update()
+    {
+
+    }
+    public void delete()
+    {
+
+    }
+    public void findUnpaidBySubscription()
+    {
+
+    }
+    public void findLastPayments()
+    {
+        
+    }
+}

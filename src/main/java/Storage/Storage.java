3a35d6caba5655b0171bede0a0ab7df0dac53512
Gourita-M <mouad.gourita@gmail.com>
2026-09-15 09:07:31 +0100

    add Storage class with basic structure and HashMap initialization

---
+++ src/main/java/Storage/Storage.java
@@ -0,0 +1,16 @@
+package Storage;
+
+import java.util.HashMap;
+
+public class Storage {
+    public static HashMap<String, String> Storage = new HashMap<>();
+    
+    public void getStorage()
+    {
+
+    }
+    public void setStorage()
+    {
+        
+    }
+}

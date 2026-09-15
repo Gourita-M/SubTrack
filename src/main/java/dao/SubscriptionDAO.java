c4184f937cec897e51d40d47c1267c1dbea45c6a
Gourita-M <mouad.gourita@gmail.com>
2026-09-15 09:22:11 +0100

    add SubscriptionDAO class with method stubs for subscription operations

---
+++ src/main/java/dao/SubscriptionDAO.java
@@ -0,0 +1,28 @@
+package dao;
+
+public class SubscriptionDAO {
+    public void creatSubscription()
+    {
+
+    }
+    public void findById(int id)
+    {
+
+    }
+    public void updateSubscription()
+    {
+
+    }
+    public void deleteSubscription()
+    {
+
+    }
+    public void findActiveSubscriptions()
+    {
+
+    }
+    public void findByType()
+    {
+        
+    }
+}

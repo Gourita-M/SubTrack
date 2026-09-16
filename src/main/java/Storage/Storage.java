94b19fd6875b5f86b8030d44b25fc0a06a52dea2
Gourita-M <mouad.gourita@gmail.com>
2026-09-16 19:00:42 +0100

    refactor: update Storage class to use specific types for storage maps

--- src/main/java/Storage/Storage.java
+++ src/main/java/Storage/Storage.java
@@ -2,15 +2,28 @@ package Storage;
 
 import java.util.HashMap;
 
-public class Storage {
-    public static HashMap<String, String> Storage = new HashMap<>();
-    
-    public void getStorage()
-    {
+import entity.SubscriptionWithoutCommitment;
+import entity.SubsctiptionWithCommitment;
 
-    }
-    public void setStorage()
+public class Storage {
+
+    public static HashMap<String, SubscriptionWithoutCommitment> storageWithout = new HashMap<>();
+    public static HashMap<String, SubsctiptionWithCommitment> storageWith = new HashMap<>();
+    
+    public static void setStoragewith(String key, SubsctiptionWithCommitment with)
     {
-        
+        storageWith.put(key, with);
+    }
+    public static void setStoragewithout(String key, SubscriptionWithoutCommitment without)
+    {
+        storageWithout.put(key, without);
+    }
+    public static HashMap<String, SubscriptionWithoutCommitment> getStorageWithout()
+    {
+        return storageWithout;
+    }
+    public static HashMap<String, SubsctiptionWithCommitment> getStorageWith()
+    {
+        return storageWith;
     }
 }

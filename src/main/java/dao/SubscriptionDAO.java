2eed65f0f3bd6a8508e4578d1a52c8fde881a1af
Gourita-M <mouad.gourita@gmail.com>
2026-09-16 18:59:16 +0100

    refactor SubscriptionDAO to add methods for creating subscriptions with and without commitment

--- src/main/java/dao/SubscriptionDAO.java
+++ src/main/java/dao/SubscriptionDAO.java
@@ -1,10 +1,46 @@
 package dao;
 
-public class SubscriptionDAO {
-    public void creatSubscription()
-    {
+import java.util.Date;
 
+import Storage.Storage;
+import entity.Status;
+import entity.SubscriptionWithoutCommitment;
+import entity.SubsctiptionWithCommitment;
+
+public class SubscriptionDAO {
+
+    public void creatSubscriptionWith(String serviceName, String monthlyAmount, Date startDate, Date endDate, Status status, long commitmentDurationMonths)
+    {
+        SubsctiptionWithCommitment subsctiptionWithCommitment = new SubsctiptionWithCommitment(serviceName, monthlyAmount, startDate, endDate, status, commitmentDurationMonths);
+        if(Storage.getStorageWith().containsKey(serviceName)){
+            System.out.println("You Already Get This Service");
+        }else{
+        Storage.setStoragewith(serviceName, subsctiptionWithCommitment);
+        }
     }
+
+    public void creatSubscriptionWithout(String serviceName, String monthlyAmount, Date startDate, Date endDate, Status status)
+    {
+        SubscriptionWithoutCommitment subscriptionWithoutCommitment = new SubscriptionWithoutCommitment(serviceName, monthlyAmount, startDate, endDate, status);
+        if(Storage.getStorageWith().containsKey(serviceName)){
+            System.out.println("You Already Get This Service");
+        }else{
+        Storage.setStoragewithout(serviceName, subscriptionWithoutCommitment);
+        }
+    }
+
+    public static void showAllSubsWith()
+    {
+        Storage.getStorageWith().values().stream()
+        .forEach(m -> System.out.println(m.toString()));
+    }
+
+    public static void showAllSubsWithout()
+    {
+        Storage.getStorageWithout().values().stream()
+        .forEach(m -> System.out.println(m.toString()));
+    }
+
     public void findById(int id)
     {
 

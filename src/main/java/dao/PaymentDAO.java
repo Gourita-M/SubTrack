bc99cb8e829405dcc1f5f6cb1f4dff99bd09c5ea
Gourita-M <mouad.gourita@gmail.com>
2026-09-17 16:34:27 +0100

    refactor: update createPayment method to include parameters for service name, due date, payment date, and status

--- src/main/java/dao/PaymentDAO.java
+++ src/main/java/dao/PaymentDAO.java
@@ -1,9 +1,23 @@
 package dao;
 
+import java.util.Date;
+
+import Storage.Storage;
+import entity.Payment;
+import entity.PaymentStatus;
+
 public class PaymentDAO {
 
-    public void createPayment(){
+    public static void createPayment(String serviceName, Date dueDate, Date paymentDate, PaymentStatus status){
+        if(Storage.getStorageWith().containsKey(serviceName)){
+            int subscriptionId = Storage.getStorageWith().get(serviceName).getId();
+            //int subscriptionId, Date dueDate, Date paymentDate, Status paymentType
 
+            Payment payment = new Payment(subscriptionId, dueDate, paymentDate, status);
+
+        }else if(Storage.getStorageWithout().containsKey(serviceName)){
+            int subscriptionId = Storage.getStorageWithout().get(serviceName).getId();
+        }
     }   
     public void findById(int id)
     {
